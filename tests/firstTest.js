2a4a9619107e139dd2a9cf723edb8e50c0d1a9d2
Domenico Gemoli <domenicogemoli@gmail.com>
2019-02-24 12:36:42 +0100

    First commit :tada:

---
+++ tests/firstTest.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'My first test case'(browser) {
+        browser
+            .url('https://news.ycombinator.com/')
+            .waitForElementVisible('.hnname')
+            .assert.containsText(".hnname", "Hacker News");
+    }
+}
